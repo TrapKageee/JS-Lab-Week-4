7ece632d40d900e40a7cddcdc8e26b96b80ae09f
TrapKageee <165345914+TrapKageee@users.noreply.github.com>
2024-04-25 01:38:57 -0500

    1st commit

---
+++ JS1VariablesAndOperations.js
@@ -0,0 +1,121 @@
+/*
+  Copyright (c) 2023 Promineo Tech
+  Author:  Promineo Tech Academic Team
+  Subject:  Variables and Operations Lab
+  FE JS1 Lab
+*/
+
+/** Key Terms:
+ *  JavaScript
+ *  GUI (Graphic User Interface)
+ *  CLI (Command Line Interface)
+ *  Variable
+ *  Data Types:
+ *   - String
+ *   - Number
+ *   - Boolean
+ *   - Array
+ *   - Undefined
+ *   - Object
+ *   - BigInt
+ *   - Null
+ *   - Symbol
+ *  Method
+ *  Git
+ *  Source Control
+ *  Operators
+ *  console.log()
+ * 
+*/
+
+/* ----------------------------------------------------- */
+// Please do not alter the existing code unless instructed to do so. 
+// Read the comments and add your code where it is specified for each question. 
+/* ----------------------------------------------------- */
+
+/*-------------------------------------------------------*/
+
+console.log(`--------------------------
+Question 1. Creating & 'console.log()'ing variables \n`);
+
+/**
+ * Instruction: Create a variable for each step, then print that variable 
+ *              to the console in the correct section.
+ *
+ * Step 1: Create a variable to hold the quantity of available seats left on a flight.
+ * Step 2: Create a variable to hold the cost of groceries at checkout
+ * Step 3: Create a variable to hold a person's middle initial
+ * Step 4: Is it Hot Outside?
+ *         Create a variable to hold a boolean, true if it's hot outside, false if it's not hot outside.
+ * Step 5: Create a variable to hold a customer's first name.
+ * Step 6: Create a variable to hold a street address.
+ * ↓ YOUR CODE HERE ↓ */
+
+console.log("Step 1:  Number of Seats on a Plane");
+var seatsAvailable = 10
+
+console.log("Step 2:  Cost of Groceries");
+var costOfGroceries = 25.99
+
+console.log("Step 3:  Middle Initial");
+var middleInitial = "M"
+
+console.log("Step 4:  Is it Hot Outside?");
+var isHotOutside = true
+
+console.log("Step 5:  First Name");
+var firstName = "Ben"
+
+console.log("Step 6:  Street Address");
+var streetAddress = '3953 W Division St'
+
+/*-------------------------------------------------------*/
+
+console.log(`--------------------------
+Question 2. Creating & 'console.log()'ing variables \n`);
+ 
+
+/** 
+ * Instruction: Use the variables we just created to solve the problems below.
+ *              Print your answers to the console.
+ * 
+ *
+ * */
+
+    console.log('Remove Plane Seats:')
+//	A customer booked 2 plane seats. Remove 2 seats from the available seats variable
+//  ↓ YOUR CODE HERE ↓
+console.log('A customer booked 2 plane seats. There are only ' + (availableFlightSeats - 2) + "seats left.")
+
+    console.log('Buy a Candy Bar:')
+//	Impulse candy bar purchase! Add 2.15 to the grocery total
+//  ↓ YOUR CODE HERE ↓
+console.log('Your total is' + (costOfGroceries + 2.15))
+
+    console.log('Fix a Birth Certificate:')
+//	A birth certificate was printed incorrectly, change the middle initial to something else
+//  ↓ YOUR CODE HERE ↓
+middleInitial = 'A'
+console.log('Middle Initial has changed to' + middleInitial)
+
+    console.log('Changing Seasons:')
+//	The season has changed, update the hot outside variable	to be opposite of what it was
+//  ↓ YOUR CODE HERE ↓
+    isHotOutside = false
+    console.log("It's no longer Summer, Fall has come!")
+
+    console.log('Full Name:')
+//	Create a new variable called full name using the customer's
+//  first name, the middle initial, and a last name of your choice
+//  ↓ YOUR CODE HERE ↓
+var fullName = firstName + " " + middleInitial + " " + "Evans"
+console.log("Your full name is: " + fullname)
+
+    console.log('Introduce a Customer:')
+//	Print a line to the console that introduces the customer 
+//	including the name, and saying that they live at the address variable
+//  ↓ YOUR CODE HERE ↓
+console.log("This is our new customer! Their name is: " + fullName + '.' + fullName + 'and they live at' + streetAddress)
+
+
+    console.log('-----------Finished------------')
